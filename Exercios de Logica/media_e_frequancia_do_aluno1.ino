099fc3b07c2cf3ff5bf79956764180728d0171fb
james <jamesantoniaeudes@gmail.com>
2025-05-08 10:42:56 -0300

    tink

---
+++ Exercios de Logica/media_e_frequancia_do_aluno1.ino
@@ -0,0 +1,38 @@
+//Calcule a média entre duas notas
+ 
+//Variaveis
+//aprovacao do aluno 
+int mediadoAluno = 10;
+int porcentagemdoAluno = 75;
+long frequenciadoAluno = 50;
+long notaMaxima = 0;
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+ void loop ()
+ {
+   Serial.println("Media do aluno");
+ while (Serial.available ());
+ nome = Serial.parseInt();
+   
+  Serial.println("porcentagem do aluno");
+ while (Serial.available ());
+ nome = Serial.parseInt();
+   
+  Serial.println("frequencia do aluno");
+ while (Serial.available ());
+ nome = Serial.parseFloat();
+   
+  Serial.println("Digite o seu nome");
+ while (Serial.available ());
+ nome = Serial.readString();
+   
+   
+ Foi dificil, eu sabia algumas coias mas a pressão tomou um pouco da 
+ minha concentração
+   
